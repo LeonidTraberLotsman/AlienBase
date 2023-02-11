a099d20b1e379f2dbb0da71849aaa68fdd1a79d7
lotsman_board <leonidt@lotsman.org>
2023-02-11 20:02:54 +0500

    vika's house

--- test6/Assets/scripts/greyGenerator/commonGreyBuilding.cs
+++ test6/Assets/scripts/greyGenerator/commonGreyBuilding.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class commonGreyBuilding : GreyBuilding
 {
-    public GameObject TablePrafab;
+    public GameObject center;
+
+    public float DistanceToWall=1;
+
     public GameObject BedPrafab;
     public GameObject chairPrafab;
-    public GameObject ShellPrafab;
+    
+
+    public List<GameObject> FurnitureToSpawn;
 
     public Transform point;
     //vika zaripova
@@ -16,8 +21,18 @@ public class commonGreyBuilding : GreyBuilding
         Debug.Log("generated");
         greatGenerator = generator;
 
-        GameObject thatone = Instantiate(TablePrafab);
-        thatone.transform.position= point.position;
+        //GameObject thatone = Instantiate(TablePrafab);
+        //thatone.transform.position= point.position;
+        spawnFurniture();
+    }
 
+    void spawnFurniture()
+    {
+        foreach (GameObject prefab in FurnitureToSpawn)
+        {
+            GameObject furniture = Instantiate(prefab);
+
+            furniture.transform.position= center.transform.position + new Vector3(Random.Range(0, DistanceToWall), Random.Range(0,DistanceToWall), Random.Range(0, DistanceToWall));
+        }
     }
 }
